fdf002970bfa8ca840991c0b4a0cf9d9f0b244a6
Adenilson Alves <140216618+denaalves@users.noreply.github.com>
2023-11-21 12:59:14 -0300

    Add files via upload

---
+++ Questão-02.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int t;
+    double a, i, v;
+
+    scanf("%d", &t);
+    getchar();
+
+    scanf("%lf", &a);
+    getchar();
+
+    scanf("%lf", &i);
+    getchar();
+
+    for (int i2 = 1; i2 <= t; i2++) {
+        v = a * (1 + i) * ((pow(1 + i, i2) - 1) / i);
+        printf("Montante ao fim do mes %d: R$ %.2lf\n", i2, v);
+    }
+
+    return 0;
+}
